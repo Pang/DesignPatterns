a6c56a69e5df3050a72b95e5b9ec6468f35c2166
cPango <ryan.beckett@outlook.com>
2020-01-22 01:22:26 +0000

    ArrayClass now works as intended and finally got omnisharp to work for Net Core 3.1!

--- Observer/StockGrabber.cs
+++ Observer/StockGrabber.cs
@@ -1,26 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Observer
 {
     public class StockGrabber : Subject
     {
-        private ArrayList<Observer> observers;
+        ArrayList observers = new ArrayList();
         private double stockPrice;
 
-        public StockGrabber(){
-            observers = new ArrayList<Observer>();
+        public StockGrabber() {
+            
         }
         public void register(Observer newObserver) {
-            observers.add(newObserver);        }
+            observers.Add(newObserver);        }
         public void unregister(Observer deleteObserver) {
             int observerIndex = observers.IndexOf(deleteObserver);
             System.Console.WriteLine("Observer: " + (observerIndex + 1));
-            observers.splice(observerIndex);
+            observers.RemoveAt(observerIndex);
         }
         public void notifyObserver() {
             foreach(Observer observer in observers) {
-                observer.update(stockprice);
+                observer.update(stockPrice);
             }
         }
-        public void setStockPrice() {
+        public void setStockPrice(double newStockPrice) {
             this.stockPrice = newStockPrice;
             notifyObserver();
         }
