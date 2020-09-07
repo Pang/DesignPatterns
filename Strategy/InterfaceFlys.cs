c6c4f2479031ba0c5570b0ea54a488afbd35ee0b
Ryan Beckett <32848753+Pang@users.noreply.github.com>
2020-09-07 09:16:28 +0100

    Update InterfaceFlys.cs

--- Strategy/InterfaceFlys.cs
+++ Strategy/InterfaceFlys.cs
@@ -4,16 +4,4 @@ namespace Strategy
     {
          string fly();
     }
-
-    class itFlies : InterfaceFlys {
-        public string fly() {
-            return "Flying high!";
-        }
-    }
-
-    class cantFly : InterfaceFlys {
-        public string fly() {
-            return "Unable to fly.";
-        }
-    }
 }
